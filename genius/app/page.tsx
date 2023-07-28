e1cbd10450005415e28ddf7c7706e59e7e598dca
raj075512 <raj075512@gmail.com>
2023-07-28 07:40:10 +0530

    use of sahdcn-ui

--- genius/app/page.tsx
+++ genius/app/page.tsx
@@ -1,9 +1,8 @@
-import Image from 'next/image'
+import { Button } from '@/components/ui/button'
+
 
 export default function Home() {
   return (
-     <p className='text-2xl text-green-500'>hello saas plat form
-
-     </p>
+     <Button variant= "outline" size="lg"> click me </Button>
   )
 }
